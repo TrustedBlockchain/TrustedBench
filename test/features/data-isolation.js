d1c74a5d356f9bd0cc10cedd67c56bafedbe0aff
fanjiahe <fanjiahe@ziggurat.cn>
2018-07-21 02:48:43 +0000

    adjust the structure

--- test/features/data-isolation.js
+++ test/features/data-isolation.js
@@ -11,74 +11,77 @@ var query = require("../../benchmar/simple/query.js");
 test('\n\n*** 初始化ZigLedger网络 ***\n\n', (t) => {
     global.tapeObj = t;
     blockchain.init();
+    testSuite();
     t.end()
 });
 
-test('\n\n*** #64 数据存储隔离性 ***\n\n', (t) => {
-    let openContext = blockchain.getContext("open");
-    open.init(blockchain,openContext)
-    open.run();
-    blockchain.releaseContext(openContext);
-    let queryContext = blockchain.getContext("query");
-    query.init(blockchain,openContext)
-    query.run();
-    blockchain.releaseContext(queryContext);
-    let queryContextDifferentChannel = blockchain.getContext("query");
-    queryContextDifferentChannel.channel = "yourchannel";
-    query.init(blockchain,queryContextDifferentChannel)
-    query.run();
-    blockchain.releaseContext(queryContextDifferentChannel);
-    t.end()
-});
-
-test('\n\n*** #65 并行业务互不干扰 ***\n\n', (t) => {
-    t.pass("同时发送两笔交易");
-    let openContext = blockchain.getContext("open");
-    open.init(blockchain,openContext)
-    open.run();
-    open.run();
-    blockchain.releaseContext(openContext);
-    t.end()
-});
-
-test('\n\n*** #66 私有信息的加密性 ***\n\n', (t) => {
-    t.pass("tls传输");
-    t.pass("私有信息密文存储");
-    t.end()
-});
-
-test('\n\n*** #67 数据传输方式 ***\n\n', (t) => {
-    let queryContext = blockchain.getContext("query");
-    let channel = queryContext.channel;
-    let peers = channel.getPeers();
-	for (let i = 0; i < peers.length; i++) {
-        t.pass("有效节点传输" + peers[i]._url);
-	}    
-    query.init(blockchain,queryContext)
-    query.run();
-    blockchain.releaseContext(queryContext);
-    t.end()
-});
-
-test('\n\n*** #68 查询和操作数据保密性 ***\n\n', (t) => {
-    let queryContext = blockchain.getContext("query");
-    let channel = queryContext.channel;
-    let peers = channel.getPeers();
-	for (let i = 0; i < peers.length; i++) {
-        peers[i]._url = "grpcs://localhost:7061";
-	}    
-    query.init(blockchain,queryContext)
-    query.run();
-    blockchain.releaseContext(queryContext);
-    t.end()
-});
-
-test('\n\n*** #69 用户数据解密 ***\n\n', (t) => {
-    t.pass("user和password failed");
-    t.end()
-});
-
-test('\n\n*** #70 隐私保护方案 ***\n\n', (t) => {
-    t.pass("side database");
-    t.end()
-});
+function testSuite(){
+    test('\n\n*** #64 数据存储隔离性 ***\n\n', (t) => {
+        let openContext = blockchain.getContext("open");
+        open.init(blockchain,openContext)
+        open.run();
+        blockchain.releaseContext(openContext);
+        let queryContext = blockchain.getContext("query");
+        query.init(blockchain,openContext)
+        query.run();
+        blockchain.releaseContext(queryContext);
+        let queryContextDifferentChannel = blockchain.getContext("query");
+        queryContextDifferentChannel.channel = "yourchannel";
+        query.init(blockchain,queryContextDifferentChannel)
+        query.run();
+        blockchain.releaseContext(queryContextDifferentChannel);
+        t.end()
+    });
+    
+    test('\n\n*** #65 并行业务互不干扰 ***\n\n', (t) => {
+        t.pass("同时发送两笔交易");
+        let openContext = blockchain.getContext("open");
+        open.init(blockchain,openContext)
+        open.run();
+        open.run();
+        blockchain.releaseContext(openContext);
+        t.end()
+    });
+    
+    test('\n\n*** #66 私有信息的加密性 ***\n\n', (t) => {
+        t.pass("tls传输");
+        t.pass("私有信息密文存储");
+        t.end()
+    });
+    
+    test('\n\n*** #67 数据传输方式 ***\n\n', (t) => {
+        let queryContext = blockchain.getContext("query");
+        let channel = queryContext.channel;
+        let peers = channel.getPeers();
+    	for (let i = 0; i < peers.length; i++) {
+            t.pass("有效节点传输" + peers[i]._url);
+    	}    
+        query.init(blockchain,queryContext)
+        query.run();
+        blockchain.releaseContext(queryContext);
+        t.end()
+    });
+    
+    test('\n\n*** #68 查询和操作数据保密性 ***\n\n', (t) => {
+        let queryContext = blockchain.getContext("query");
+        let channel = queryContext.channel;
+        let peers = channel.getPeers();
+    	for (let i = 0; i < peers.length; i++) {
+            peers[i]._url = "grpcs://localhost:7061";
+    	}    
+        query.init(blockchain,queryContext)
+        query.run();
+        blockchain.releaseContext(queryContext);
+        t.end()
+    });
+    
+    test('\n\n*** #69 用户数据解密 ***\n\n', (t) => {
+        t.pass("chaincode 加密和解密数据");
+        t.end()
+    });
+    
+    test('\n\n*** #70 隐私保护方案 ***\n\n', (t) => {
+        t.pass("side database");
+        t.end()
+    });
+}
