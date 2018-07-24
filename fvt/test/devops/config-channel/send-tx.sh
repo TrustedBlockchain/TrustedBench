4d59b52100f4e9d74ff4e4c3977e9b2757428619
qiang0723 <wqiang0723@gmail.com>
2018-07-24 17:54:53 +0800

    update config channel

--- fvt/test/devops/config-channel/send-tx.sh
+++ fvt/test/devops/config-channel/send-tx.sh
@@ -8,8 +8,8 @@ while true
 do
     echo "Send a query transaction"
     docker exec cli peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}'
-    sleep 2
+    sleep 1
     echo "Send a transfer transaction"
     docker exec cli peer chaincode invoke -o orderer.example.com:7050  --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -c '{"Args":["invoke","a","b","1"]}' -i "1000000000" -z bc4bcb06a0793961aec4ee377796e050561b6a84852deccea5ad4583bb31eebe
-    sleep 2
+    sleep 1
 done
