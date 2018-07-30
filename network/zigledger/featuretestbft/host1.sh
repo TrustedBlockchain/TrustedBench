f6b0424c3c816588fcd99049c5c980af925453a9
grapebaba <281165273@qq.com>
2018-07-30 12:32:49 +0800

    Add tendermint script
    Signed-off-by: grapebaba <281165273@qq.com>

---
+++ network/zigledger/featuretestbft/host1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/node_key.json crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/testchainid;
+
+cp crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/node_key.json crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/mychannel;
+
+cp crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/priv_validator.json crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/testchainid;
+
+cp crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/priv_validator.json crypto-config/ordererOrganizations/example.com/orderers/orderer0.example.com/tendermint/mychannel;
